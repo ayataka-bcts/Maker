6bfe0aa001844a45c1dea136466a4a36e2d3dc62
DESKTOP-EPU2FDF\ayataka <scene.of.bloodshed232@gmail.com>
2019-04-22 22:19:58 +0900

    [add]ゲーム開始前のカウントダウンの追加  - チュートリアルの差し込み  - ゲーム開始前のカウントダウンの実装

---
+++ Assets/Scripts/Games/TutorialPannel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPannel : MonoBehaviour {
+
+    private Animator _animator;
+
+    [SerializeField]
+    private Text countDown;
+    [SerializeField]
+    private GameObject fadeInPannel;
+
+    IEnumerator FadeIn()
+    {
+        fadeInPannel.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        fadeInPannel.SetActive(false);
+    }
+
+    IEnumerator CountDown()
+    {
+        _animator.SetTrigger("_FadeOut");
+
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        countDown.gameObject.SetActive(true);
+        countDown.text = "3";
+
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        countDown.text = "2";
+
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        countDown.text = "1";
+
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        countDown.text = "START !";
+
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        Time.timeScale = 1.0f;
+        Destroy(this.gameObject);
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        _animator = this.GetComponent<Animator>();
+
+        StartCoroutine(FadeIn());
+        Time.timeScale = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(CountDown());
+        }	
+	}
+}
